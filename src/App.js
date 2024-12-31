db71483b8e48cf1d1e04a56be617d78347206295
양승빈 (AI IoT플랫폼개발TF) <seungbin.yang@kt.com>
2024-12-31 22:07:08 +0900

    카드 추가 기능 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Traffic from './components/Traffic';
 import Stock from './components/Stock';
 import Weather from './components/Weather';
+import LoginPage from './components/LoginPage';
 import './css/App.css';
 
 const App = () => {
     return (
         <Router>
-            <div className="app">
-                <Sidebar />
-                <div className="content">
-                    <Routes>
-                        <Route path="/Traffic" element={<Traffic />} />
-                        <Route path="/Stock" element={<Stock />} />
-                        <Route path="/Weather" element={<Weather />} />
-                    </Routes>
-                </div>
-            </div>
+            <Routes>
+                {/* 로그인 페이지 */}
+                <Route path="/" element={<LoginPage />} />
+
+                {/* 로그인 후 다른 페이지 */}
+                <Route
+                    path="/*"
+                    element={
+                        <div className="app">
+                            <Sidebar />
+                            <div className="content">
+                                <Routes>
+                                    <Route path="/Traffic" element={<Traffic />} />
+                                    <Route path="/Stock" element={<Stock />} />
+                                    <Route path="/Weather" element={<Weather />} />
+                                </Routes>
+                            </div>
+                        </div>
+                    }
+                />
+            </Routes>
         </Router>
     );
 };
